8f4f32629cd1266b369304d99f43c2815dc44d19
Andrew Woo <andrew.woo@verizon.net>
2023-03-09 13:45:04 -0500

    :sparkle: feat: removing pregenerated code and styles. added constants folder with question types and 3 questions as part of the assessment

---
+++ src/constants/questions.tsx
@@ -0,0 +1,35 @@
+type questionType = "radio" | "checkbox" | "freeText";
+
+type questionRule = {
+  customerType?: string[];
+  location?: string[];
+  purchased?: string[];
+};
+
+type question = {
+  prompt: string;
+  responses: string[];
+  type: questionType;
+  rules: questionRule;
+};
+
+export const question1: question = {
+  prompt: "How did you hear about us?",
+  responses: ["Facebook", "Google", "TikTok", "Instagram"],
+  type: "radio",
+  rules: { customerType: ["New"] },
+};
+
+export const question2: question = {
+  prompt: "Do you own any of the following?",
+  responses: ["Dumbbells", "Medicine Ball", "Jump Rope", "Stationary Bike"],
+  type: "checkbox",
+  rules: { customerType: ["Returning"], location: ["Idaho", "Wisconsin"] },
+};
+
+export const question3: question = {
+  prompt: "What made you decide to buy the Black Jeans today?",
+  responses: [],
+  type: "freeText",
+  rules: { purchased: ["Black Jeans"] },
+};
